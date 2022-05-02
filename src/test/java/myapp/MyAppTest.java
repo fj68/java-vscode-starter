d41d84e7a79ec5ac84caddd8b232dcbcbfb3ae56
Shintaro K <fj68forg@gmail.com>
2022-05-02 22:13:42 +0900

    Update MyAppTest.java

--- src/test/java/myapp/MyAppTest.java
+++ src/test/java/myapp/MyAppTest.java
@@ -1,29 +1,24 @@
 package myapp;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+// import org.junit.jupiter.api.AfterAll;
+// import org.junit.jupiter.api.BeforeAll;
+// import static org.junit.jupiter.api.Assertions.AssertEqual;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class MyAppTest {
   /*
-  @BeforeAll
-  static void beforeAll() {
-    System.out.println("Start MyAppTest");
-  }
-  */
+   * @BeforeAll static void beforeAll() { System.out.println("Start MyAppTest"); }
+   */
 
   /*
-  @AfterAll
-  static void afterAll() {
-    System.out.println("Finish MyAppTest");
-  }
-  */
+   * @AfterAll static void afterAll() { System.out.println("Finish MyAppTest"); }
+   */
 
   @Test
+  @DisplayName("Test A Method")
   void testAMethod() {
-    System.out.println("Run testAMethod");
-    assertEquals(0, 0, "Validate 0 = 0")
+    fail("Must fail.");
   }
 }
